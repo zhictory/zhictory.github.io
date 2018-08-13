263ac8319ae720e320b785b6d5899e6940ac98b4
zhangzhensheng <zhictory@qq.com>
2018-08-13 09:33:27 +0800

    Updated 优化全局样式和交互

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 # Zhictory 的技术博客
 
 环境：
+
 - jekyll 3.7.3
 - ruby 2.3.3
 - gem 2.5.2
 
 构建：
-```
+
+```shell
 jekyll serve
 ```
 
+移动端联调：
+
+- PC 开户移动热点，移动端连接该热点。
+- PC 开启本地服务器，使用 browser-sync 监听本地服务器，移动端访问指定 IP，如：
+    ```shell
+    browser-sync start --proxy '127.0.0.1:4000'
+    ```
+
 模板：
+
 - [long-haul](https://github.com/brianmaierjr/long-haul)
 
 文档：
+
 - [Jekyll 文档](http://jekyll.bootcss.com/)
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,23 +17,33 @@ $( document ).ready(function() {
 // 点击显示大图
 document.querySelector('.content').addEventListener('click', function(event){
   
-  let ele = event.target;
+  const ele = event.target;
   if (ele.nodeName === 'IMG') {
-    let div = document.createElement('div');
-    let img = new Image();
+    const div = document.createElement('div');
+    const img = new Image();
     img.src = ele.src;
     div.appendChild(img);
     document.documentElement.appendChild(div);
     div.classList.add('zoom');
+    div.id = 'j_zoom';
     setTimeout(function(){ img.classList.add('img__animation'); }, 0);
   }
 
 }, false);
-// 点击关闭大图
+// 点击空白关闭大图
 document.documentElement.addEventListener('click', function(event){
   
-  let ele = event.target;
+  const ele = event.target;
   if (event.target.classList.contains('zoom')) {
+    document.documentElement.removeChild(ele);
+  }
+  
+}, false);
+// 按 ESC 关闭大图
+document.documentElement.addEventListener('keyup', function(event) {
+  
+  const ele = document.querySelector('#j_zoom');
+  if (document.documentElement.contains(ele)) {
     document.documentElement.removeChild(ele);
   }
 
